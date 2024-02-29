f966e6919e4e1ee2eb930b063f8e6151fa9a58bd
88ROKAS88 <rokas8misevicius@gmail.com>
2024-02-29 19:42:49 +0200

    Update Player.js
    small improvements.

--- survivor/js/Player.js
+++ survivor/js/Player.js
@@ -1,15 +1,15 @@
 class Player {
   constructor(game) {
     this.game = game;
-    this.positionY = 0;
+    this.positionY = 350;
     this.changeY = 0;
-    this.positionX = 0;
+    this.positionX = 350;
     this.changeX = 0;
 
     this.speed = 2;
   }
   draw() {
-    game.display.ctx.fillStyle = "red";
+    game.display.ctx.fillStyle = "green";
     game.display.ctx.fillRect(this.positionX, this.positionY, 50, 50);
   }
 }
