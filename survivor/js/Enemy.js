c848350b251c267a01dafa9f422f122454cfec5b
88ROKAS88 <rokas8misevicius@gmail.com>
2024-03-03 14:17:12 +0200

    Update Enemy.js
    display enemies health.

--- survivor/js/Enemy.js
+++ survivor/js/Enemy.js
@@ -1,16 +1,25 @@
 class Enemy {
   constructor(game) {
     this.game = game;
+
+    // #### POSITION ####
     this.positionY = 0;
     this.changeY = 0;
     this.positionX = 0;
     this.changeX = 0;
 
+    // #### STATS ####
     this.speed = 1;
+    this.health = 5;
+  }
+  displayHP() {
+    game.display.ctx.fillStyle = "red";
+    game.display.ctx.fillRect(this.positionX, this.positionY, this.health, 2);
   }
   draw() {
-    game.display.ctx.fillStyle = "red";
+    game.display.ctx.fillStyle = "blue";
     game.display.ctx.fillRect(this.positionX, this.positionY, 50, 50);
+    this.displayHP();
   }
   ai() {
     if (game.player.positionY > this.positionY) {
