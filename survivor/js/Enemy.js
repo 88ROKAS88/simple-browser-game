f36ac5e1f545d5f85210213838569674414e68ca
88ROKAS88 <rokas8misevicius@gmail.com>
2024-04-01 14:09:25 +0300

    Update Enemy.js
    refactoring.

--- survivor/js/Enemy.js
+++ survivor/js/Enemy.js
@@ -8,6 +8,16 @@ class Enemy {
     this.positionX = 0;
     this.changeX = 0;
 
+    // #### ORIENTATION ####
+    this.orientation = 0;
+
+    this.left = false;
+    this.right = false;
+    this.up = false;
+    this.down = false;
+
+    this.change = false;
+
     // #### STATS ####
     this.size = 50;
     this.speed = 1;
@@ -22,17 +32,76 @@ class Enemy {
     game.display.ctx.fillRect(this.positionX, this.positionY, 50, 50);
     this.displayHP();
   }
-  ai() {
+  findPlayer() {
+    this.change = false;
     if (game.player.positionY > this.positionY + 50 + 25) {
-      this.positionY += this.speed;
-    } else if (game.player.positionY < this.positionY - 50 - 25) {
-      this.positionY -= this.speed;
+      if (!this.down == true) {
+        this.change = true;
+
+        this.down = true;
+        this.up = false;
+      }
+    } else if (!this.down == false) {
+      this.change = true;
+
+      this.down = false;
+    }
+
+    if (game.player.positionY < this.positionY - 50 - 25) {
+      if (!this.up == true) {
+        this.change = true;
+
+        this.up = true;
+        this.down = false;
+      }
+    } else if (!this.up == false) {
+      this.change = true;
+
+      this.up = false;
     }
 
     if (game.player.positionX > this.positionX + 50 + 25) {
-      this.positionX += this.speed;
-    } else if (game.player.positionX < this.positionX - 50 - 25) {
+      if (!this.right == true) {
+        this.change = true;
+
+        this.right = true;
+        this.left = false;
+      }
+    } else if (!this.right == false) {
+      this.change = true;
+
+      this.right = false;
+    }
+
+    if (game.player.positionX < this.positionX - 50 - 25) {
+      if (!this.left == true) {
+        this.change = true;
+
+        this.left = true;
+        this.right = false;
+      }
+    } else if (!this.left == false) {
+      this.change = true;
+
+      this.left = false;
+    }
+  }
+  move() {
+    if (this.up) {
+      this.positionY -= this.speed;
+    }
+    if (this.down) {
+      this.positionY += this.speed;
+    }
+    if (this.left) {
       this.positionX -= this.speed;
     }
+    if (this.right) {
+      this.positionX += this.speed;
+    }
+  }
+  ai() {
+    this.findPlayer();
+    this.move();
   }
 }
