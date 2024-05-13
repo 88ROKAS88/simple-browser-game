21a8d6c02c45181553998756367fd8adab59ee00
88ROKAS88 <rokas8misevicius@gmail.com>
2024-05-13 22:02:04 +0300

    Update Attack2.js
    attack2 execution and visuals.

--- survivor/js/Attack2.js
+++ survivor/js/Attack2.js
@@ -1,5 +1,28 @@
 class Attack2 extends Attack {
   constructor(playerAtt, attackerPosY, attackerPosX, attackerOrientation) {
     super(playerAtt, attackerPosY, attackerPosX, attackerOrientation);
+    this.timer = 60;
+  }
+
+  execute() {
+    this.timer -= 1;
+    // if (this.timer == 20) {
+    //   this.collision();
+    // }
+    if (this.timer == 1) {
+      game.attacks.push(
+        new Attack(true, this.positionY, this.positionX + 50, 0)
+      );
+    }
+  }
+
+  draw() {
+    game.display.ctx.fillStyle = "orange";
+    game.display.ctx.fillRect(
+      this.positionX,
+      this.positionY,
+      this.size,
+      this.size
+    );
   }
 }
