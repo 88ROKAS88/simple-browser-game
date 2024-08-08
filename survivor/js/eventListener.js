8362f242e3121d92e0dceb7b6583819027ce6146
88ROKAS88 <rokas8misevicius@gmail.com>
2024-08-08 22:49:15 +0300

    bug fix
    movement attacking on pause bug.

--- survivor/js/eventListener.js
+++ survivor/js/eventListener.js
@@ -1,48 +1,51 @@
 window.addEventListener("keydown", (e) => {
   // console.log(e.key);
-
-  if (e.key == "ArrowUp") {
-    if (!game.inputs.down) game.inputs.up = true;
-  }
-  if (e.key == "ArrowDown") {
-    if (!game.inputs.up) game.inputs.down = true;
-  }
-  if (e.key == "ArrowLeft") {
-    if (!game.inputs.right) game.inputs.left = true;
-  }
-  if (e.key == "ArrowRight") {
-    if (!game.inputs.left) game.inputs.right = true;
-  }
-  if (e.key == "1") {
-    game.player.attack(e.key);
-  }
-  if (e.key == "2") {
-    game.player.attack(e.key);
-  }
-  if (e.key == "3") {
-    game.player.attack(e.key);
-  }
-  if (e.key == "4") {
-    game.player.attack(e.key);
+  if (!game.pauseVar) {
+    if (e.key == "ArrowUp") {
+      if (!game.inputs.down) game.inputs.up = true;
+    }
+    if (e.key == "ArrowDown") {
+      if (!game.inputs.up) game.inputs.down = true;
+    }
+    if (e.key == "ArrowLeft") {
+      if (!game.inputs.right) game.inputs.left = true;
+    }
+    if (e.key == "ArrowRight") {
+      if (!game.inputs.left) game.inputs.right = true;
+    }
+    if (e.key == "1") {
+      game.player.attack(e.key);
+    }
+    if (e.key == "2") {
+      game.player.attack(e.key);
+    }
+    if (e.key == "3") {
+      game.player.attack(e.key);
+    }
+    if (e.key == "4") {
+      game.player.attack(e.key);
+    }
+    game.player.findOrientation();
   }
   if (e.key == "p") {
     game.pause(260, "PAUSE");
   }
-  game.player.findOrientation();
 });
 
 window.addEventListener("keyup", (e) => {
-  if (e.key == "ArrowUp") {
-    game.inputs.up = false;
+  if (!game.pauseVar) {
+    if (e.key == "ArrowUp") {
+      game.inputs.up = false;
+    }
+    if (e.key == "ArrowDown") {
+      game.inputs.down = false;
+    }
+    if (e.key == "ArrowLeft") {
+      game.inputs.left = false;
+    }
+    if (e.key == "ArrowRight") {
+      game.inputs.right = false;
+    }
+    game.player.findOrientation();
   }
-  if (e.key == "ArrowDown") {
-    game.inputs.down = false;
-  }
-  if (e.key == "ArrowLeft") {
-    game.inputs.left = false;
-  }
-  if (e.key == "ArrowRight") {
-    game.inputs.right = false;
-  }
-  game.player.findOrientation();
 });
